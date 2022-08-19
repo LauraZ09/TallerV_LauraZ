3ad92753a8ba1a97721a5e22e22ebd114fb67af0
Laura Zuluaga <lazuluagamo@unal.edu.co>
2022-08-19 15:58:51 -0500

    Update main.c
    Se realizó el primer ejercicio.

---
+++ EdadHexBin/Src/main.c
@@ -0,0 +1,23 @@
+/**
+ ******************************************************************************
+ * @file           : Tarea 1. Mi edad en otros números
+ * @author         : Laura Alejandra Zuluaga
+ * @brief          :????? preguntar qué es el brief
+ ******************************************************************************
+ ******************************************************************************
+ */
+
+//#include <stdint.h> Esto es necesario?
+
+int main(void) //Se crea la función main.
+
+{
+
+
+
+
+
+	while (1) { //Se le pone el ciclo infinito
+		;
+	}
+}
