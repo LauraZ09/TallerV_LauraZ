404f0a028c870190a081d42af7d76ab3acc60e7a
Laura Zuluaga <lazuluagamo@unal.edu.co>
2022-08-19 16:02:39 -0500

    Update main.c
    Se realiza el primer ejercicio

--- EdadHexBin/Src/main.c
+++ EdadHexBin/Src/main.c
@@ -9,6 +9,30 @@
 
 //#include <stdint.h> Esto es necesario?
 
+/*Ejercicio 1: Definiendo algunas variables.
+ *
+ * A cerca de la elección de los tamaños de las variables:
+ *
+ * 1. Variable my_number_of_years: Esta es la variable donde se almacenará mi cantidad
+ * de años. Se le asigna un tamaño char, ya que con 8 bits es más que suficiente para
+ * la edad de cualquier persona (con 8 bits se pueden almacenar en la variable números
+ * entre el 0 y el 255).
+ *
+ * 2. Variable days_per_year: En esta variable se almacenará la cantidad de días por año.
+ * Se le asigna un tamaño short, ya que cada año cuenta con 365 días, al ser el número
+ * 365 mayor a 255, se necesita un tamaño más grande que el char, por tanto se asigna
+ * con un tamaño de dato short.
+ *
+ * 3. Variables seconds_per_hour: En esta variable se almacenará la cantidad de segundos
+ * por hora. Se le asigna a la variable un tamaño char, ya que al ser 60 segundos por
+ * hora, 8 bits serán suficientes para almacenar esta variable.
+ */
+
+unsigned char my_number_of_years = 0;  //Variable para Mi número de años.
+unsigned short days_per_year = 0;      //Variable para la cantidad de días por año.
+unsigned char seconds_per_hour = 0;    //Variable para la cantidad de segundos por hora.
+
+
 int main(void) //Se crea la función main.
 
 {
