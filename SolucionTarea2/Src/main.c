df8d0d68cb4e1653ee7abc4a8e79b0b18e7701b3
Laura Zuluaga <lazuluagamo@unal.edu.co>
2022-09-02 19:04:34 -0500

    Creación proyecto SolucionTarea2
    Se crea el Proyecto y se copian los archivos de drivers necesarios.

---
+++ SolucionTarea2/Src/main.c
@@ -0,0 +1,20 @@
+/**
+ ******************************************************************************
+ * @file           : Solución Tarea 2
+ * @author         : Laura Alejandra Zuluaga Moreno
+ ******************************************************************************
+ *En el siguiente documento se encuentra la solución a la Tarea 2
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
