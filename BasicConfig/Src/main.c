54d7851f0581326fa8904ed0d0b3f333520b63cd
Laura Zuluaga <lazuluagamo@unal.edu.co>
2022-09-16 12:22:13 -0500

    usart

--- BasicConfig/Src/main.c
+++ BasicConfig/Src/main.c
@@ -21,20 +21,10 @@
 #include "GPIOxDriver.h"
 #include "BasicTimer.h"
 
-#define RED   	 1
-#define GREEN 	 2
-#define BLUE     3
-#define MAGENTA  4
-
 /*Función Principal del programa*/
 
 BasicTimer_Handler_t handlerTimer3 = { 0 };
 GPIO_Handler_t handlerBlinky = { 0 };
-uint32_t counter_i_1 = 0;
-uint32_t counter_i_2 = 0;
-
-void BasicPWM (uint32_t dutyCicle, uint32_t period, uint32_t repetitions);
-//void NeoPixelLEDColorOn (uint8_t color);
 
 int main(void) {
 
@@ -55,128 +45,9 @@ int main(void) {
 
 	BasicTimer_Config(&handlerTimer3);
 
-	BasicPWM (8, 2, 2);
-	BasicPWM (4, 1, 3);
-	NeoPixelLEDColorOn(1);
-
 	while (1) {
 	}
 }
 
-void BasicPWM (uint32_t upCicles, uint32_t downCicles, uint32_t repetitions) {
-
-	counter_i_1 = 0;
-	counter_i_2 = 0;
-
-	//upCicles es el número de interrupciones que va a estar arriba
-	//El period es el número de interrupciones de la señal completa
-	uint32_t period = upCicles + downCicles;
-	//repetitions: número de períodos de la señal
-
-
-	while (counter_i_2 < repetitions){
-
-		if (counter_i_1 <= upCicles){
-			GPIO_WritePin(&handlerBlinky, SET);}
-
-		else if (counter_i_1 > upCicles && counter_i_1 <= period){
-			GPIO_WritePin(&handlerBlinky, RESET);}
-
-		else if (counter_i_1 > period){
-			counter_i_1 = 0;
-			counter_i_2++;}
-	}
-}
-
-/*void NeoPixelLEDColorOn (uint8_t color) { //Está con porcentaje
-
-	if (color == 1) {
-		BasicPWM (64, 25000, 8);
-		counter_i_1 = 0;
-		counter_i_2 = 0;
-
-    	BasicPWM (32, 25000, 8);
-		counter_i_1 = 0;
-		counter_i_2 = 0;
-
-    	BasicPWM (32, 25000, 8);
-		counter_i_1 = 0;
-		counter_i_2 = 0;
-
-    	BasicPWM  (0, 25000, 1);
-		counter_i_1 = 0;
-		counter_i_2 = 0;
-	}
-
-	else if (color == 2) {
-    	BasicPWM (64, 25000, 8);
-    	BasicPWM (32, 25000, 8);
-    	BasicPWM (32, 25000, 8);
-    	BasicPWM  (0, 25000, 1);
-	}
-
-	else if (color == 3) {
-    	BasicPWM (32, 25000, 8);
-    	BasicPWM (32, 25000, 8);
-    	BasicPWM (64, 25000, 8);
-    	BasicPWM  (0, 25000, 1);
-	}
-
-	else if (color == 4) {
-    	BasicPWM (64, 25000, 8);
-    	BasicPWM (32, 25000, 8);
-    	BasicPWM (64, 25000, 8);
-    	BasicPWM  (0, 25000, 1);
-	}
-}
-
 void BasicTimer3_Callback(void) {
-	//GPIOxTooglePin(&handlerBlinky)
-	counter_i_1++;}
-
-
-
-
-	/*switch (color) {
-
-	    case 1:
-	    	BasicPWM (64, 25000, 8);
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM  (0, 25000, 1);
-	    break;
-
-	    case 2:
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM (64, 25000, 8);
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM  (0, 25000, 1);
-	      break;
-
-	    case 3:
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM (64, 25000, 8);
-	    	BasicPWM  (0, 25000, 1);
-	      break;
-
-	    case 4:
-	    	BasicPWM (64, 25000, 8);
-	    	BasicPWM (32, 25000, 8);
-	    	BasicPWM (64, 25000, 8);
-	    	BasicPWM  (0, 25000, 1);
-	      break;
-
-	    default:
-	    	__NOP();
-	      break;*/
-
-
-
-
-
-
-
-
-
-
+	GPIOxTooglePin(&handlerBlinky);}
