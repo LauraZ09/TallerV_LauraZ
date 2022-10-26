edcda19d90b6de7b03be08d184641ab4ac073a8b
Laura Zuluaga <lazuluagamo@unal.edu.co>
2022-10-26 04:04:13 -0500

    Update main.c

--- ProyectoFinal/Src/main.c
+++ ProyectoFinal/Src/main.c
@@ -14,12 +14,12 @@
 #include "GPIOxDriver.h"
 #include "BasicTimer.h"
 
-#define TIM_FREQ 40   //Frecuencia del Timer en MHz
-#define T0H 16        //Este es el valor que se debe poner en el CCx para alcanzar 0.4u
-#define T1H 32        //Este es el valor que se debe poner en el CCx para alcanzar 0.8u
-#define T0L 34	      //Este es el valor que se debe poner en el CCx para alcanzar 0.85u
-#define T1L 18        //Este es el valor que se debe poner en el CCx para alcanzar 0.45u
-#define TRESET 2000   //Este es el valor que se debe poner en el CCx para alcanzar 50u
+#define TIM_FREQ 	40   	  //Frecuencia del Timer en MHz
+#define T0H 		16        //Este es el valor que se debe poner en el CCx para alcanzar 0.4u
+#define T1H 		32        //Este es el valor que se debe poner en el CCx para alcanzar 0.8u
+#define T0L 		34	      //Este es el valor que se debe poner en el CCx para alcanzar 0.85u
+#define T1L 		18        //Este es el valor que se debe poner en el CCx para alcanzar 0.45u
+#define TRESET 		2000      //Este es el valor que se debe poner en el CCx para alcanzar 50u
 
 PWM_Handler_t handlerPWMTimer 	        = { 0 };  //Handler para el PWM (Timer)
 BasicTimer_Handler_t handlerBlinkyTimer = { 0 };  //Handler para el BlinkyTimer
@@ -28,27 +28,28 @@ GPIO_Handler_t handlerMCO_2  			= { 0 };  //Handler para el PIN de salida del Cl
 GPIO_Handler_t handlerPWMOutput 		= { 0 };  //Handler para la salida del PWM
 
 
+uint8_t flag 			= 0;
+uint16_t position 		= 0;
+uint16_t low_CCR1 		= 0;
+uint16_t low_ARR 		= 0;
+uint16_t high_CCR1 		= 0;
+uint16_t high_ARR 		= 0;
+uint16_t treset_ARR 	= 0;
+long double period		= 0;
+uint8_t mask 			= 0b10000000;
 
 uint8_t LED_data[180]; //En este arreglo se almacenan los números para la intensidad del RGB
 					   //el arreglo es de 180 porque son 60 LEDS y 3 bytes por LED
-uint16_t position;
-uint8_t mask = 0B10000000;
-uint8_t lastbit;
-long double period;
-uint16_t low_CCR1, low_ARR, high_CCR1, high_ARR, treset_ARR;
 
 void Neopixel_setup(void);
 void show_neopixels(void);
-uint8_t neopixel_transmitting(void);
-
-
-
 void initSystem(void); //Función para inicializar el sistema
+uint8_t neopixel_transmitting(void);
 
 int main(void)
 {
-	Neopixel_setup(); //Se cargan los valores
 	setTo40M();
+	Neopixel_setup(); //Se cargan los valores
 	initSystem();
 	startPwmSignal(&handlerPWMTimer);
 	enableOutput(&handlerPWMTimer);
@@ -57,17 +58,12 @@ int main(void)
 	{
 		//Primero se llena el arreglo con una secuencia consecutiva de verde, rojo y azul
 		for (uint8_t i = 0; i < 180; i += 9) {
-			LED_data[i] = 25;      //Se usan valores bajos para no gastar mucha corriente
-			LED_data[i + 4] = 25;  //Se usan valores bajos para no gastar mucha corriente
-			LED_data[i + 8] = 25;  //Se usan valores bajos para no gastar mucha corriente
+			LED_data[i] = 25; //Se usan valores bajos para no gastar mucha corriente
+			LED_data[i + 4] = 25; //Se usan valores bajos para no gastar mucha corriente
+			LED_data[i + 8] = 25; //Se usan valores bajos para no gastar mucha corriente
 		}
 
 		show_neopixels();  //transmit the data to the neopixel strip.
-
-		HAL_Delay(1000);
-
-
-
 	}
 
 	return 0;
@@ -75,18 +71,50 @@ int main(void)
 
 void Neopixel_setup(void){
 
-	period = 1 / TIM_FREQ;
-	low_CCR1 = T0H;
-	low_ARR = 50;
-	high_CCR1 = T1H;
-	high_ARR = 50;
-	treset_ARR = 2000;
+	period 		= 1 / TIM_FREQ;
+	low_CCR1	= T0H;
+	low_ARR 	= 50;
+	high_CCR1 	= T1H;
+	high_ARR 	= 50;
+	treset_ARR 	= TRESET;
 }
 
 void show_neopixels(void){
-	position = 0;           //set the interupt to start at first byte
-	lastbit = 0;
-	mask = 0B10000000;      //set the interupt to start at second bit
+	position = 0;
+	mask     = 0b10000000;
+}
+
+void BasicTimer2_Callback(void) {
+	GPIOxTooglePin(&handlerBlinkyPin);
+	}
+
+void BasicTimer5_Callback(void){
+	if (position < sizeof(LED_data)) {
+
+		if (LED_data[position] & mask) {
+			updateDuttyCycle(&handlerPWMTimer, high_CCR1);
+			updateFrequency(&handlerPWMTimer, high_ARR);
+		}
+
+		else {
+			updateDuttyCycle(&handlerPWMTimer, low_CCR1);
+			updateFrequency(&handlerPWMTimer, low_ARR);
+		}
+
+		if (mask == 1) {
+			mask = 0b10000000;
+			position++;
+		}
+
+		else {
+			mask = mask >> 1;
+		}
+
+	}
+	else {
+		updateDuttyCycle(&handlerPWMTimer, 0);
+		updateFrequency(&handlerPWMTimer, TRESET);
+	}
 }
 
 void initSystem(void) {
@@ -144,29 +172,5 @@ void initSystem(void) {
 
 }
 
-void BasicTimer2_Callback(void) {
-	GPIOxTooglePin(&handlerBlinkyPin);
-	}
 
-void BasicTimer5_Callback(void){
-
-		if(position < sizeof(LED_data)){
-
-			if(LED_data[position] & mask){
-				TIM4->CCR1 = high_CCR1;
-				TIM4->ARR  = high_ARR;
-			}else{
-				TIM4->CCR1 = low_CCR1;
-				TIM4->ARR  = low_ARR;
-			}
-			if(mask == 1){
-				mask = 0B10000000;
-				position++;
-			}else mask = mask >> 1;
-
-		}else{
-			TIM4->CCR1 = 0; //set to zero so that pin stays low
-			TIM4->ARR = TRESET; //set to timing for reset LEDs
-		}
-}
 
